9b8e10d830d5ab2178060479532aa699b75a0c87
Calum Sieppert <sieppertcalum@gmail.com>
2019-12-22 11:43:26 -0700

    fixup! Cabin scene beginning

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Layout } from '../components/Layout'
+import { CabinScene } from '../components/CabinScene'
 
 export default function IndexPage({ location }) {
   return (
